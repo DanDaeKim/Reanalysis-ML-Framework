a9aaaa9f3a2d6e04f1b682a5887dc87e93c51024
DanDaeKim <131207269+DanDaeKim@users.noreply.github.com>
2023-04-24 10:51:03 -0700

    Create clustering.py

---
+++ clustering.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+
+# Load the dataset
+data = pd.read_csv('path/to/your/dataset.csv')
+
+# Preprocess the dataset (e.g., handling missing values, encoding categorical variables)
+# The specific preprocessing steps will depend on your chosen dataset
+
+# Split the dataset into features (X) and target variable (y)
+X = data.drop('target_variable', axis=1)
+y = data['target_variable']
+
+# The code for this step will depend on the original study's statistical analysis method
+# This step may involve creating new features or transforming existing ones to improve the performance of the machine learning models
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Instantiate the models
+dt = DecisionTreeRegressor()
+rf = RandomForestRegressor()
+gb = GradientBoostingRegressor()
+
+# Train the models
+dt.fit(X_train, y_train)
+rf.fit(X_train, y_train)
+gb.fit(X_train, y_train)
+
+# Make predictions
+dt_preds = dt.predict(X_test)
+rf_preds = rf.predict(X_test)
+gb_preds = gb.predict(X_test)
+
+# Evaluate the models
+dt_mse = mean_squared_error(y_test, dt_preds)
+rf_mse = mean_squared_error(y_test, rf_preds)
+gb_mse = mean_squared_error(y_test, gb_preds)
+
+dt_r2 = r2_score(y_test, dt_preds)
+rf_r2 = r2_score(y_test, rf_preds)
+gb_r2 = r2_score(y_test, gb_preds)
+
+print("Decision Tree - MSE: {}, R2: {}".format(dt_mse, dt_r2))
+print("Random Forest - MSE: {}, R2: {}".format(rf_mse, rf_r2))
+print("Gradient Boosting - MSE: {}, R2: {}".format(gb_mse, gb_r2))
+
+# Compare the performance of the models and visualize the results using bar plots
+models = ['Decision Tree', 'Random Forest', 'Gradient Boosting']
+mse_values = [dt_mse, rf_mse, gb_mse]
+r2_values = [dt_r2, rf_r2, gb_r2]
+
+plt.bar(models, mse_values)
+plt.xlabel('Models')
+plt.ylabel('Mean Squared Error')
+plt.title('Model Comparison - MSE')
+plt.show()
+
+plt.bar(models, r2_values)
+plt.xlabel('Models')
+plt.ylabel('R-squared')
+plt.title('Model Comparison - R2')
+plt.show()
+
+# Further interpretation of the results will depend on the specific dataset and models used
+
