3732f1320a92c7bc00a6a8efedd57947c9540c65
DanDaeKim <131207269+DanDaeKim@users.noreply.github.com>
2023-04-25 21:32:25 -0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+
+def plot_mse(models, mse_values):
+    plt.bar(models, mse_values)
+    plt.xlabel('Models')
+    plt.ylabel('Mean Squared Error')
+    plt.title('Model Comparison - MSE')
+    plt.show()
+
+def plot_r2(models, r2_values):
+    plt.bar(models, r2_values)
+    plt.xlabel('Models')
+    plt.ylabel('R-squared')
+    plt.title('Model Comparison - R2')
+    plt.show()
 
